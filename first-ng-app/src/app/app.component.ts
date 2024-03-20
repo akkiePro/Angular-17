a31735aa3e52f98395bbbbe9c4648396b2c3938a
Akash Prajapati <akashprajapati1849@gmail.com>
2024-03-20 11:26:06 +0530

    this is how you should use any new/user-defined component

--- first-ng-app/src/app/app.component.ts
+++ first-ng-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserProfileComponent],
   // templateUrl: './app.component.html',
   // instead of creating new HTML file, we can write html code in template also.
   template: `
