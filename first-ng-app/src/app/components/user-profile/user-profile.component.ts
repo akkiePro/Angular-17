4b1476cc759bcd31e0f58f8970be6cd8fe83e4d1
Akash Prajapati <akashprajapati1849@gmail.com>
2024-03-20 12:42:51 +0530

    using *ng directives which was being used before Angular 17.

--- first-ng-app/src/app/components/user-profile/user-profile.component.ts
+++ first-ng-app/src/app/components/user-profile/user-profile.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
