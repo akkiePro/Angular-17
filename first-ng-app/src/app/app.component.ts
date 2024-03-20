ed777b8c6b6ad64a2ad8d5e5e20ad7556a89bbdf
Akash Prajapati <akashprajapati1849@gmail.com>
2024-03-20 11:13:35 +0530

    Introduction of template code without HTML file

--- first-ng-app/src/app/app.component.ts
+++ first-ng-app/src/app/app.component.ts
@@ -5,7 +5,11 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  // instead of creating new HTML file, we can write html code in template also.
+  template: `
+  <h1>Hello world from TS.</h1>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
