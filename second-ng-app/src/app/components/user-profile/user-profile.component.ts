407fe3f752e8b5312b4201feb2d625f5908b2ad8
Akash Prajapati <akashprajapati1849@gmail.com>
2024-03-20 13:17:54 +0530

    alias for @Input() decorator

--- second-ng-app/src/app/components/user-profile/user-profile.component.ts
+++ second-ng-app/src/app/components/user-profile/user-profile.component.ts
@@ -8,5 +8,5 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
-  @Input() name = ""
+  @Input({alias: "userName"}) name = ""
 }
