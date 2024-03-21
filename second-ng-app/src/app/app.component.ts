18fca94c3b44ec40f6d134d0456d8bceba01d330
Akash Prajapati <akashprajapati1849@gmail.com>
2024-03-21 11:29:03 +0530

    @Input(): parent to child multiple data pass using @for and *ngFor

--- second-ng-app/src/app/app.component.ts
+++ second-ng-app/src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserProfileComponent } from "./components/user-profile/user-profile.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, UserProfileComponent]
+    imports: [RouterOutlet, UserProfileComponent, CommonModule]
 })
 export class AppComponent {
   title = 'second-ng-app';
+
+  users = [
+    {name: "Naman", salary: 48000, isSingle: true},
+    {name: "Manan", salary: 25000, isSingle: false},
+    {name: "Chaman", salary: 15000, isSingle: true},
+    {name: "Aman", salary: 90000, isSingle: false}
+  ]
 }
